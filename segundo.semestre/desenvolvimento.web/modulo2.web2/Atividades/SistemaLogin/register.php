b68b7f57e609bfe86228801bb91d665324e9bd0c
JoãoPedrinho013 <135490211+JoaoPedrinho013@users.noreply.github.com>
2025-10-05 16:52:39 -0300

    form login/register

--- segundo.semestre/desenvolvimento.web/modulo2.web2/Atividades/SistemaLogin/register.php
+++ segundo.semestre/desenvolvimento.web/modulo2.web2/Atividades/SistemaLogin/register.php
@@ -25,7 +25,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro</title>
+    <title>Projeto web 3 | Cadastro</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
--- segundo.semestre/desenvolvimento.web/modulo2.web2/Atividades/SistemaLogin/user.php
+++ segundo.semestre/desenvolvimento.web/modulo2.web2/Atividades/SistemaLogin/user.php
@@ -18,7 +18,7 @@ if (isset($_GET['logout'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Área do Usuário</title>
+    <title>Projeto web 3 | Usuário</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
