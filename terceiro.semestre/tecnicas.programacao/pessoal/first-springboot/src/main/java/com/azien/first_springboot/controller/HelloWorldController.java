dc99c946ca0004cf93a5df6540906dc1d06390c7
João Pedro <joaopedroliveira98@gmail.com>
2026-03-04 10:38:19 -0300

    Adicionando projeto Spring Boot

---
+++ terceiro.semestre/tecnicas.programacao/pessoal/first-springboot/src/main/java/com/azien/first_springboot/controller/HelloWorldController.java
@@ -0,0 +1,8 @@
+package com.azien.first_springboot.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController /*Existe o @Controller e @ResponseBody, esse @RestController usa os dois, pq eu posso usar só o @Controller caso n seja necessario uma response Json/XML*/
+public class HelloWorldController {
+
+}
