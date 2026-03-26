e9494b52ddf5c6ccef0b9ef24f25466c775a00bd
João Pedro <joaopedroliveira98@gmail.com>
2026-03-26 20:52:59 -0300

    atividade java

---
+++ terceiro.semestre/tecnicas.programacao/desafios/open-meteo-clima/src/main/java/br/edu/fatecpg/open_meteo_clima/OpenMeteoClimaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fatecpg.open_meteo_clima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenMeteoClimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenMeteoClimaApplication.class, args);
+	}
+
+}
---
+++ terceiro.semestre/tecnicas.programacao/desafios/open-meteo-clima/src/test/java/br/edu/fatecpg/open_meteo_clima/OpenMeteoClimaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatecpg.open_meteo_clima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenMeteoClimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
