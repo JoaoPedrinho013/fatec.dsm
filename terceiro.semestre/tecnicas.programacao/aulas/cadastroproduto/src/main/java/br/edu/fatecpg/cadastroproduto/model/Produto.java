84b505d570c64bd4156d2c906d91aaa33266af65
Joao Pedro <joaopedroliveira98@gmail.com>
2026-03-31 17:43:18 -0300

    aula java

---
+++ terceiro.semestre/tecnicas.programacao/aulas/cadastroproduto/src/main/java/br/edu/fatecpg/cadastroproduto/model/Produto.java
@@ -0,0 +1,43 @@
+package br.edu.fatecpg.cadastroproduto.model;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private String categoria;
+    private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Produto() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
