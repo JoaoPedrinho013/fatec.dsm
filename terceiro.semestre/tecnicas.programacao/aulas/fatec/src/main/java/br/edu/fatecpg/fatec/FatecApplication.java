209a79f9f4ac1ccd24aad6b17d1e355200d898ef
João Pedro <joaopedroliveira98@gmail.com>
2026-04-28 15:53:28 -0300

    atv java jpa hibernate

---
+++ terceiro.semestre/tecnicas.programacao/aulas/fatec/src/main/java/br/edu/fatecpg/fatec/FatecApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fatecpg.fatec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FatecApplication.class, args);
+	}
+
+}
