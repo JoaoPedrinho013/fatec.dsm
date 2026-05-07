032d7a7e8f323f06283c171a9027c016cde54ddf
João Pedro <joaopedroliveira98@gmail.com>
2026-05-07 18:03:05 -0300

    aula de django

--- terceiro.semestre/desenvolvimento.web/aulas/07.05.26/app/forms.py
+++ terceiro.semestre/desenvolvimento.web/aulas/07.05.26/app/forms.py
@@ -1,41 +1,37 @@
 from django import forms
 from app.models import Categoria, Contato, Produto
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.forms import User
-
-#Aq é para criar um formulario no modelo categoria 
+from django.contrib.auth.models import User  
 
 class FormUsuario(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
         labels = {
-            'username' : 'Nome',
-            'email' : 'E-mail'
+            'username': 'Nome',
+            'email': 'E-mail'
         }
 
-class FormEditarUsuario(forms.ModelForm):
+class FormEditarUsuario(forms.ModelForm): 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email']
         labels = {
-            'username' : 'Nome',
-            'email' : 'E-mail'
+            'username': 'Nome',
+            'email': 'E-mail'
         }
-
 
 class FormCategoria(forms.ModelForm):
     class Meta:
-        model = Categoria 
+        model = Categoria
         fields = ['nome']
 
 class FormContato(forms.ModelForm):
     class Meta:
         model = Contato
-        fields = ['nome','email','assunto','mensagem']
+        fields = ['nome', 'email', 'assunto', 'mensagem']
 
 class ProdutoForm(forms.ModelForm):
     class Meta:
         model = Produto
-        fields = ['nome','imagem','quantidade','preco','categoria']
-
+        fields = ['nome', 'imagem', 'quantidade', 'preco', 'categoria']
--- terceiro.semestre/desenvolvimento.web/aulas/07.05.26/projeto/settings.py
+++ terceiro.semestre/desenvolvimento.web/aulas/07.05.26/projeto/settings.py
@@ -121,3 +121,9 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/login/' 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
